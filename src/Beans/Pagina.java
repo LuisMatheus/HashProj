f849702426f6306af8968a0b2820758605f0b598
Luís Tavares <luismatheus.tavares@gmail.com>
2020-04-08 21:54:05 -0300

    update

---
+++ src/Beans/Pagina.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Matheus
+ */
+public class Pagina {
+    public HashMap<Integer,String> tuplas = new HashMap<>();
+}
